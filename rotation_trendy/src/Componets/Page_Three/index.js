16d8bbd642c902be8575a1d4f4656b185fd057c4
Jetender <g.jetender@gmail.com>
2022-02-10 02:39:55 -0500

    sections and pages

---
+++ rotation_trendy/src/Componets/Page_Three/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+import Section  from '../Sections';
+
+function PageThree (){
+
+    return(
+
+        <Section section = 'sectionThree'>
+
+  <div className='pageThree'>
+
+        <h1>Page3</h1>
+
+    </div>
+
+        </Section>
+  
+
+
+
+    )
+
+}
+
+
+
+export default PageThree;
